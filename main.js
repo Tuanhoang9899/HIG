79b52435590fe592e5aa0e503dea66c0a5336ca4
Tuanhv <tuanhv@nhanh.vn>
2025-06-27 00:55:21 +0700

    update

--- main.js
+++ main.js
@@ -72,4 +72,14 @@ $(document).ready(function () {
 
         lastScrollTop = currentScroll;
     });
+
+    $('.mobile-menu-toggle').click(function() {
+        $('.mobile-menu').addClass('active');
+        $('.mobile-menu-overlay').addClass('active');
+    });
+
+    $('.close-menu, .mobile-menu-overlay').click(function() {
+        $('.mobile-menu').removeClass('active');
+        $('.mobile-menu-overlay').removeClass('active');
+    });
 });
