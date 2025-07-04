6859dbea68e40eb954d1c455b26e1b7e4629b714
Tuanhv <tuanhv@nhanh.vn>
2025-07-04 23:21:15 +0700

    update

--- main.js
+++ main.js
@@ -60,6 +60,23 @@ $(document).ready(function () {
     }
 
 
+    const backToTop = $('#backToTop')
+    const amountScrolled = 300
+
+    $(window).scroll(() => {
+        $(window).scrollTop() >= amountScrolled
+            ? backToTop.fadeIn('fast')
+            : backToTop.fadeOut('fast')
+    })
+
+    backToTop.click(() => {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 600)
+        return false
+    })
+
+
     // $('.news_slide').slick({
     //     dots: false,
     //     arrows: false,
